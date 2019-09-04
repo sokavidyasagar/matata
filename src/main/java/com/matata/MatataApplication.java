714492c6456affd597626d56a52e271c29720533
vidyasagar <sokavidyasagar@gmail.com>
2019-09-04 22:52:46 +0530

    added by vidya sagar

---
+++ src/main/java/com/matata/MatataApplication.java
@@ -0,0 +1,13 @@
+package com.matata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatataApplication.class, args);
+	}
+
+}
